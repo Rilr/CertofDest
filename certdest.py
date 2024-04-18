a65407c0a9bf99a4eb3617a095bc8cef62832a9a
Rilr <Jorgelschn@gmail.com>
2024-04-18 16:25:21 -0400

    Initial commit

---
+++ certdest.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import PySimpleGUI as sg
+import docx
+import os
+import sys
+
+layout = [[sg.Text('HDD Data:')],
+          [sg.InputText(key = 'inpath'), 
+           sg.FileBrowse(file_types =[( 'XLSX Files','*.xlsx')])],
+          
+          [sg.Text('Where to Save:')],
+          [sg.InputText(key = 'outpath'),
+           sg.FolderBrowse()]
+          
+          [sg.Button('Ok'), sg.Button('Cancel')]]
+
+window = sg.Window('HDD Data to Word', layout)
+event, values = window.read()
+outpathFile = values['outpath'] + '/cert-of-destruction.docx'
+
+hddData = {
+    'file_path': values['inpath'],
+    'brand': 'Brand',
+    'model': 'Model',
+    'size': 'Capacity',
+    'serial': 'Serial',
+    'mdate': 'Manufacture Date',
+    'ddate': 'Destruction Date',
+    'dmethod': 'Destruction Method',
+}
+
+def gatherData():
+    
+def templateCreation():
+    
+def dataInsertion():
+    
+def main():
+    gatherData()
+    templateCreation()
+    dataInsertion()
+
+if __name__ == '__main__':
+    main()
